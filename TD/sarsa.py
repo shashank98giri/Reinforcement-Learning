221dcd806ffac4260eef0e0ac65ae30b36c350f2
shashank98giri <g.shashank700@gmail.com>
2018-06-09 02:42:14 +0530

    Applied GLIE in place of epsilon greedy

--- TD/sarsa.py
+++ TD/sarsa.py
@@ -4,13 +4,15 @@ import itertools
 from collections import defaultdict,namedtuple
 from matplotlib import pyplot as plt
 import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+
 Stats=namedtuple("Stats",["episode_length","episode_rewards"])
 
 plt.style.use('ggplot')
 class Sarsa(object):
     def __init__(self,num_episodes,num_actions,epsilon,gamma):
         #setting class variables
-        self.q=defaultdict(lambda:np.random.rand(num_actions))
+        self.q=defaultdict(lambda:np.zeros(num_actions))
 
         self.epsilon=epsilon
         self.gamma=gamma
@@ -18,25 +20,25 @@ class Sarsa(object):
         self.num_episodes=num_episodes
         self.env=CliffWalking(self.num_actions)
 
-    def policy(self,state):
-        A=np.ones(self.num_actions)*self.epsilon/self.num_actions
-        A[np.argmax(self.q[state])]+=1-self.epsilon
+    def policy(self,state,epsilon):
+        A=np.ones(self.num_actions)*epsilon/self.num_actions
+        A[np.argmax(self.q[state])]+=1-epsilon
         return A
 
     def learner(self,alpha=0.5):
         stats=Stats(episode_length=np.zeros(self.num_episodes),
         episode_rewards=np.zeros(self.num_episodes))
-
+        epsilon=self.epsilon
         for episode in range(self.num_episodes):
 
             state=self.env.reset()
-            action_prob=self.policy(state)
+            action_prob=self.policy(state,epsilon/(episode+1))
             action=np.random.choice(np.arange(self.num_actions),p=action_prob)
 
             for i in itertools.count():
 
                 next_state,reward,done=self.env.step(action)
-                next_action_prob=self.policy(next_state)
+                next_action_prob=self.policy(next_state,epsilon/(episode+1))
                 next_action=np.random.choice(np.arange(self.num_actions),p=next_action_prob)
 
                 #TD error
@@ -52,28 +54,36 @@ class Sarsa(object):
                     break
                 state=next_state
                 action=next_action
+
         return self.q,stats
 
     def plot(self,s):
-        fig=plt.figure(figsize=(12,7))
-        plt.plot(s.episode_length)
-        plt.xlabel('episode count')
-        plt.ylabel(' episode_length')
-        plt.title('Episode length')
-        plt.show(fig)
+        # fig=plt.figure(figsize=(12,7))
+        # plt.plot(s.episode_length)
+        # plt.xlabel('episode count')
+        # plt.ylabel(' episode_length')
+        # plt.title('Episode length')
+        # plt.show(fig)
         fig=plt.figure(figsize=(10,7))
         rewards=pd.Series(s.episode_rewards).rolling(10,min_periods=10).mean()
         plt.plot(rewards)
         plt.xlabel('episode count')
         plt.ylabel('reward')
-        plt.title('Episode reward')
+        plt.title('Reward over time')
+        plt.show(fig)
+        fig=plt.figure(figsize=(10,7))
+        plt.plot(np.cumsum(s.episode_length),np.arange(len(s.episode_length)))
+        plt.xlabel('episode count')
+        plt.ylabel('Episode')
+        plt.title('Episode per time step')
         plt.show(fig)
 
 
-num_episodes=200
+
+num_episodes=1000
 num_actions=4
 epsilon=0.1
-gamma=1.0
+gamma=0.9
 sarsa=Sarsa(num_episodes,num_actions,epsilon,gamma)
 Q,stats=sarsa.learner()
 sarsa.plot(stats)
